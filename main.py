6afe8a67a14f11d1f7e93ecde7246053c7bc3b23
StasKostohlodow <staskostohlodow@gmail.com>
2023-04-14 09:38:14 +0300

    delete spaces in code

--- main.py
+++ main.py
@@ -16,15 +16,12 @@ height = int(input("Enter height (0 for auto): "))
 
 for file in files:
     image = Image.open(open_path+file)
-
     new_width = width
     new_height = height
     if width==0:
         new_width = int(height*image.size[0]/image.size[1])
     elif height==0:
         new_height = int(width*image.size[1]/image.size[0])
-        
-
     resized_image = image.resize((new_width, new_height))
     resized_image.save(saver_path+file)
 
