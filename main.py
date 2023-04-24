b11e7bcf299417ca59ff34f1a2026eaf4167d2a6
StasKostohlodow <staskostohlodow@gmail.com>
2023-04-24 11:02:38 +0300

    Сhanged the paths to relative ones instead of absolute ones

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import os
 
 open_folder = 'img'
 saver_folder = 'resize-img'
-open_path = f"{os.path.dirname(__file__)}\\{open_folder}\\"
-saver_path = f"{os.path.dirname(__file__)}\\{saver_folder}\\"
 
-if not os.path.exists(saver_folder):
-    os.makedirs(saver_folder)
 
-files = os.listdir(open_path)
+
+files = os.listdir(open_folder)
 
 width = int(input("Enter width (0 for auto): "))
 height = int(input("Enter height (0 for auto): "))
 
+if not os.path.exists(saver_folder):
+    os.makedirs(saver_folder)
+    
 for file in files:
-    image = Image.open(open_path+file)
+    image = Image.open(open_folder+'\\'+file)
     new_width = width
     new_height = height
     if width==0:
@@ -23,6 +23,7 @@ for file in files:
     elif height==0:
         new_height = int(width*image.size[1]/image.size[0])
     resized_image = image.resize((new_width, new_height))
-    resized_image.save(saver_path+file)
+    resized_image.save(saver_folder+'\\'+file)
 
+input("Enter any to close programm")
 
